defd0bdd5a8bdafe0bac9d81d22212fec3973779
Dr Ron <rjljr2@gmail.com>
2025-03-17 16:29:11 -0700

    start chapter3

---
+++ chapter3.py
@@ -0,0 +1,8 @@
+# %% [markdown]
+# ## Recommended exercises:
+#  3.2, 3.3, 3.9 (model solutions availablefor these)
+#
+# Also look at [Course Exercises](https://avehtari.github.io/BDA_course_Aalto/assignments/assignment3.html)
+
+# My plan is to look at #2 from the course, and #3.3 from the book. One is multinomial, the other is a normal distribution. 
+# %%
