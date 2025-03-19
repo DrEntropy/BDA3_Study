9d0ad7cac5283f66a8b1303ef399a69525acb769
DrEntropy <DrEntropy@users.noreply.github.com>
2025-03-19 14:42:32 -0700

    Finish chapter 3

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # BDA3_Study
 A place for me to save notes and exercises for the book Bayesian Data Analysis (3rd Edition)
 
+I will be following the Course notes [here](https://avehtari.github.io/BDA_course_Aalto/)
 
 ## Note on the code
 
--- chapter3.py
+++ chapter3.py
@@ -111,9 +111,75 @@ compute_odds(1,1)
 # 
 
 # %% [markdown]
+# ## Exercise #3.3 (from book)
+# 
+# Estimation from two independent experiments: an experiment was performed on the
+# effects of magnetic fields on the flow of calcium out of chicken brains. Two groups
+# of chickens were involved: a control group of 32 chickens and an exposed group of 36
+# chickens. One measurement was taken on each chicken, and the purpose of the experiment
+# was to measure the average flow $\mu_c$ in untreated (control) chickens and the average flow
+# $\mu_t$ in treated chickens. The 32 measurements on the control group had a sample mean of
+# 1.013 and a sample standard deviation of 0.24. The 36 measurements on the treatment
+# group had a sample mean of 1.173 and a sample standard deviation of 0.20.
+# 
+# 
+# a) Assume the mesurements were taken at random from a normal distribution for each variable with mean $\mu_c$ and $\mu_t$ and standard deviation $\sigma_c$ and $\sigma_t$.   Assume prior uniform on $\mu_c, \mu_t, \log \sigma_c and \log \sigma_t$.  Compute the marginal posterior distribution of $\mu_c, \mu_t $
+# 
+# b) What is the posterior difference in means? Plot the histogram compute 95% posterior interval for $\mu_t - \mu_c$
+
+# %% [markdown]
+# ### Answer: 
+# 
+# The book gives the posterior distribution for this case:
+# 
+# $$
+# \frac{\bar{y} - \mu}{s/\sqrt{n}} \sim t_{n-1}
+# $$
+# 
+# where $\bar{y}$ is the sample mean, $s$ is the sample standard deviation, and $n$ is the sample size.   For the instant case we are given the sample means and sample standard deviations, so the posterior distributions are self evident.
+# 
+# To sample from these we will define a function:
 # 
 
 # %%
+import numpy as np
+from scipy.stats import t
+import pandas as pd
+import matplotlib.pyplot as plt
 
+# %%
+np.std(t.rvs(3, size=10000))
+
+# %%
+rng = np.random.default_rng(42)
+def sample_post(y_c, y_t, n_c,n_t,sig_c,sig_t,rng, n= 10):
+    draws_c = t.rvs(n_c - 1,random_state=rng, size=n)* sig_c / np.sqrt(n_c) + y_c
+    draws_t = t.rvs(n_t - 1,random_state=rng, size=n) * sig_t / np.sqrt(n_t) + y_t
+    return   draws_c, draws_t 
+
+
+# %%
+
+samples = sample_post(1.013, 1.173, 32, 36, 0.24, 0.20, rng, n= 10000)
+
+df = pd.DataFrame({"Control": samples[0], "Treatment": samples[1], "Treatment Effect": samples[1] - samples[0]})
+df_long = df.melt(value_vars=["Control", "Treatment", "Treatment Effect"], var_name="Group", value_name="Value")
+df_long 
+
+# %%
+import seaborn as sns
+sns.histplot(data=df_long, hue="Group", x="Value", kde=True, bins=30)
+
+# %%
+np.percentile(df["Treatment Effect"], [2.5, 97.5])
+
+# %%
+np.mean(df["Treatment Effect"] < 0)
+
+# %% [markdown]
+# There is > 99% probability that the treatment effect is positive, and the 95% posterior credible interval is from 0.05 to 0.27.
+
+# %% [markdown]
+# 
 
 
