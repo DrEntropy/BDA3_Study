5ed14161fbea042cc9e5b7161d532f769a8518ae
Dr Ron <rjljr2@gmail.com>
2025-04-27 16:00:37 -0700

    minor edit

--- sleepmodel.R
+++ sleepmodel.R
@@ -1,6 +1,9 @@
 data(sleepstudy, package = "lme4")
 library(brms)
 
+# Note this is essentially assignment 6 in week 7, or parts of it
+
+## Random intercepts model
 
 ( priorsRI <- c(
   prior(
@@ -31,8 +34,10 @@ library(brms)
 fit <- brms::brm(bf(Reaction ~ Days +  (1 | Subject),family = "gaussian", center=FALSE),
                  prior = priorsRI, data = sleepstudy)
 
+# we note that this gives for population level effect of days on reaction as 10.45 +- 0.79
 
 ## Random slopes version
+
 ( priorsRS <- c(
   prior(
     normal(250, 100),
@@ -52,7 +57,7 @@ fit <- brms::brm(bf(Reaction ~ Days +  (1 | Subject),family = "gaussian", center
 
   ),
   prior(
-      normal(0,50),
+      normal(0,20),
       class = 'sd',
       coef = 'Days',
       group = 'Subject'
@@ -74,3 +79,8 @@ modelf <- bf(Reaction ~ Days +  ( 1 + Days | Subject),family = "gaussian", cente
 fit2 <- brms::brm(modelf,
                   prior = priorsRS,
                    data = sleepstudy)
+
+# For random slopes version , the estimated slope on Days was 10.42 +-1.72 .
+# Subject specific effect sd on days was 6.6
+
+
